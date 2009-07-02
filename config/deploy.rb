6f33dd18886429e37732eae8e8fd010b51aec530
Flatsoft Admin <support@flatsoft.com>
2009-07-02 19:01:56 +0400

    Fixup deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :repository, 'git@github.com:fs/ruby-lh2bc.git'
 server 'flatsoft-rails.flatsoft.ru', :app
 
 set :user, 'admin'
-set :deploy_to, "/var/www/apps/#{application}"
+set :deploy_to, "/var/www/rails/#{application}"
 set :use_sudo, false
 set :branch, 'master'
 set :scm, :git
@@ -13,7 +13,7 @@ set :deploy_via, :remote_cache
 after 'deploy:symlink', 'deploy:update_crontab'
 
 namespace :deploy do
-  [:start, :stop, :restart].each do |t|
+  [:finalize_update, :migrate, :start, :stop, :restart].each do |t|
     desc "#{t} task is a no-op with mod_rails"
     task t, :roles => :app do ; end
   end
@@ -24,7 +24,7 @@ namespace :deploy do
   end
 
   desc "Update the crontab file"
-  task :update_crontab, :roles => :db do
+  task :update_crontab do
     run "cd #{current_path} && whenever -f config/cron.rb --update-crontab #{application}"
   end
 end
