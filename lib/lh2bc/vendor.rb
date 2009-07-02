fce5fba849fb75d446b9b2893c3c0890f3c66e38
Timur Vafin <timur.vafin@flatsoft.com>
2009-07-02 19:24:31 +0400

    Frozen activesupport

--- lib/lh2bc/vendor.rb
+++ lib/lh2bc/vendor.rb
@@ -1,6 +1,7 @@
-%w(basecamp-rb lighthouse-api addressable-2.1.0 activeresource-2.3.2).each do |lib|
+%w(basecamp-rb lighthouse-api addressable-2.1.0 activeresource-2.3.2 activesupport-2.3.2).each do |lib|
   $:.unshift(File.expand_path(File.dirname(__FILE__) + "/vendor/#{lib}/lib"))
 end
 
 require 'basecamp'
 require 'lighthouse'
+require 'activesupport'
